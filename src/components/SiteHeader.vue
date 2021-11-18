896170dc3f9c7d05faa22884f1ce198723e52284
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-18 12:49:31 +0100

    Creata struttura di base dell'esercizio
    Creata struttura facendo uso di Vue CLI, ed inseriti nella cartella components quattro file vue, SiteHeader, SiteMain, SiteFooter e SearchBar, mentre nella cartella assets inserita la cartella scss, che continere due file scss. Ed infine importati Bootstrap e Axios.

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="SiteHeader">
+		<h1 class="p-1 text-center">BoolFlix</h1>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/common.scss";
+
+</style>
---
+++ src/components/SiteMain.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="SiteMain">
+		<div class="container">
+			<SearchBar />
+		</div>
+	</div>
+</template>
+
+<script>
+import SearchBar from "./SearchBar.vue";
+
+export default {
+	components: {
+		SearchBar,
+	},
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/common.scss";
+</style>
