f9987c1178cc1f82756223dad95c87b280b6f383
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-18 16:20:34 +0100

    Creato file vue (SearchMovie)
    Dal precedente file SearchBar, inserita struttura di base di vue, un tag form con dentro un label, un input e un button.

---
+++ src/components/SearchMovie.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="searchMovie d-flex justify-content-center m-5">
+		<form>
+			<label for="movie"></label>
+			<input
+				class="p-2 text-left"
+				type="text"
+				name="movie"
+				id="movie"
+				placeholder="Inserire nome film"
+				v-model="searchMovie"
+			/>
+			<button
+				class="w-auto ms-3 p-2 border-primary"
+				@click="$emit('search-movie', searchMovie)"
+			>
+				Inizia >
+			</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			searchMovie: "",
+		};
+	},
+};
+</script>
+
+<style lang="scss">
+button {
+	border: none;
+	color: #fff;
+	background-color: red;
+}
+</style>
