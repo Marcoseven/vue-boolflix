3c42cea611ffb9ddb7fa4c87df31a1f77ba34ca1
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-18 16:18:28 +0100

    Aggiunte modifiche file vue (SiteMain)
    Inserite row con le col-2 con il ciclo for ed altro nell'export default.

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,17 +1,66 @@
 <template>
 	<div id="SiteMain">
 		<div class="container">
-			<SearchBar />
+			<h2 class="text-center">Film, serie TV e tanto altro. Senza limiti.</h2>
+			<div class="row">
+				<SearchMovie @search-movie="search" />
+				<div class="col-2" v-for="movie in films" :key="movie.id">
+					<div class="movie text-center">
+						<h3>Titolo: {{ movie.title.toUpperCase() }}</h3>
+						<div>
+							<h4>Titolo originale: {{ movie.original_title }}</h4>
+							<h4>Lingua originale: {{ movie.original_language }}</h4>
+							<h4>Voto: {{ movie.vote_count }}</h4>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import SearchBar from "./SearchBar.vue";
+import SearchMovie from "./SearchMovie.vue";
+import axios from "axios";
 
 export default {
 	components: {
-		SearchBar,
+		SearchMovie,
+	},
+	data() {
+		return {
+			movies: "",
+			loading: true,
+			error: "",
+			api_key: "5eba0868299db17ce62c13e86462a76c",
+			queryUserInput: "",
+		};
+	},
+	mounted() {
+		setTimeout(this.callApi, 1000);
+	},
+	methods: {
+		callApi() {
+			axios
+				.get(
+					"https://api.themoviedb.org/3/search/movie?api_key=" +
+						this.api_key +
+						"&language=en-US&query=" +
+						this.queryUserInput +
+						"&page=1&include_adult=false"
+				)
+				.then((r) => {
+					console.log(r.data);
+					this.movies = r.data.response;
+					this.loading = false;
+				})
+				.catch((e) => {
+					console.log(e, "OPS!");
+					this.error = `'OPS!' ${e}`;
+				});
+		},
+	},
+	computed: {
 	},
 };
 </script>
