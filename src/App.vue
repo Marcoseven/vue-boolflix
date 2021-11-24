5206278a70326d885073e199f935351e2c4ec1e0
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-24 16:50:53 +0100

    Aggiunte modifiche file vue (App)
    Aggiunte diverse modifiche generiche.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
 		<SiteHeader />
-		<div class="container">
+		<div class="container-input">
 			<div class="row mt-5">
 				<form class="mb-5 text-center">
 					<label for="movie"></label>
@@ -10,7 +10,7 @@
 						type="text"
 						name="movie"
 						id="movie"
-						placeholder="Inserire nome film o serie-TV"
+						placeholder="Inserire nome film o serie-tv"
 						v-model="queryInput"
 					/>
 					<button
@@ -21,73 +21,84 @@
 					</button>
 				</form>
 			</div>
-			<div class="row">
-				<div class="card" v-for="(data, key, index) in results" :key="index">
-					<div class="col-3" v-for="result in data.results" :key="result.id">
-						<div v-if="key === 'movies'">
-							<h4>Titolo film: {{ result.title }}</h4>
-							<h4>Titolo originario del film: {{ result.original_title }}</h4>
-						</div>
-						<div v-else>
-							<h4>Titolo serie-tv: {{ result.name }}</h4>
-							<h4>{{ result.original_name }}</h4>
-						</div>
-						<div>
-							<h4 v-if="getFlags(original_language)">
+		</div>
+		<div class="container-card">
+			<div
+				class="row justify-content-center flex-wrap"
+				v-for="(data, key, index) in results"
+				:key="index"
+				:class="key"
+			>
+				<div class="card" v-for="result in data.results" :key="result.id">
+					<div v-if="key === 'movies'">
+						<h4>Titolo film: {{ result.title }}</h4>
+
+						<h4>Titolo originario del film: {{ result.original_title }}</h4>
+					</div>
+					<div v-else>
+						<h4>Titolo serie-tv: {{ result.name }}</h4>
+						<h4>Titolo originario della serie-tv:{{ result.original_name }}</h4>
+					</div>
+					<div>
+						<i class="fas fa-house-user"></i>
+					</div>
+					<div>
+						<span v-if="Flags(result.original_language)">
+							<span>
 								<country-flag
 									:country="
-										result.original_language === 'it'
-											? 'it'
+										result.original_language === 'en'
+											? 'gb'
 											: result.original_language
 									"
 									size="normal"
 								/>
-							</h4>
-							<h4 v-else>
-								Lingua originale del film: {{ result.original_language }}>
-							</h4>
-							<h4>
-								Voti ottenuti:
+							</span>
+						</span>
+						<span v-else
+							>Lingua originaria del film:
+							{{ result.original_language }}
+						</span>
+					</div>
+					<div>
+						<div class="vote d-flex align-items-baseline">
+							<h4 class="pe-3">Voti ottenuti:</h4>
+							<span
+								v-for="vote in Math.round(result.vote_average / 2).toFixed(0)"
+								:key="vote.id"
+							>
+								<i class="fas fa-star"></i>
 								<span
-									v-for="vote in parseInt((result.vote_average / 2))"
+									v-for="vote in 5 -
+									Math.round(result.vote_average / 2).toFixed(0)"
 									:key="vote.id"
 								>
-									<i class="fa fa-star"></i>
-								</span>
-								<span
-									v-for="vote in 5 - (result.vote_average / 2)"
-									:key="vote.id"
-								>
-									<i class="fa fa-star"></i>
-								</span>
-							</h4>
-							<h4>Overview: {{ result.overview }}</h4>
-							<div class="image">
-								<h4>Copertina:</h4>
-								<img
-									:src="`https://image.tmdb.org/t/p/w342/` + result.poster_path"
-									alt=""
-								/>
-							</div>
+									<i class="fas fa-star"></i
+								></span>
+							</span>
+						</div>
+						<h4>Overview: {{ result.overview }}</h4>
+						<div class="image">
+							<img
+								:src="`https://image.tmdb.org/t/p/w342/` + result.poster_path"
+								alt="immagine copertina di film o serie-tv"
+							/>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<SiteMain />
 	</div>
 </template>
 
 <script>
 import SiteHeader from "./components/SiteHeader";
-import SiteMain from "./components/SiteMain";
 import axios from "axios";
 
 export default {
 	name: "App",
 	components: {
 		SiteHeader,
-		SiteMain,
 	},
 	data() {
 		return {
@@ -97,7 +108,7 @@ export default {
 			movies_url: "https://api.themoviedb.org/3/search/movie",
 			series_url: "https://api.themoviedb.org/3/search/tv",
 			results: {},
-			flags: ["it", "fr", "es", "en", "nl"],
+			flags: ["it", "fr", "es", "en", "nl", "da"],
 		};
 	},
 	methods: {
@@ -116,15 +127,15 @@ export default {
 				});
 		},
 		axiosMovies() {
-			const urlCompiledFilms = `${this.movies_url}?api_key=${this.api_key}&query=${this.movieUserInput}`;
+			const urlCompiledFilms = `${this.movies_url}?api_key=${this.api_key}&query=${this.queryInput}`;
 			return axios.get(urlCompiledFilms);
 		},
 		axiosSeries() {
-			const urlCompiledSeries = `${this.movies_url}?api_key=${this.api_key}&query=${this.movieUserInput}`;
+			const urlCompiledSeries = `${this.series_url}?api_key=${this.api_key}&query=${this.queryInput}`;
 			return axios.get(urlCompiledSeries);
 		},
-		getFlags(flag) {
-			return this.flags.includes(flag);
+		Flags(iconFlags) {
+			return this.flags.includes(iconFlags);
 		},
 	},
 };
@@ -133,39 +144,45 @@ export default {
 <style lang="scss">
 @import "../node_modules/bootstrap/scss/bootstrap.scss";
 @import "./assets/scss/common.scss";
-@import "./assets/scss/variables.scss";
 
 .container {
 	overflow-x: auto;
 }
 
 .row {
+	padding: 0 0.5rem;
+	margin: 0 -0.5rem;
 	background-color: #000;
 }
 
 .card {
-	width: calc(100% / 5);
+	width: calc(100% / 4);
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	border: 1px solid #fff;
+	margin: 0.5rem;
+	padding: 0.5rem;
 	background-color: #000;
-	.col-3 {
-		height: auto;
-		width: 100%;
-		border: 1px solid #fff;
-		margin: 0 0.5rem;
-		padding: 1rem;
-		background-color: #000;
-		color: #fff;
-		.content {
-			height: 100%;
-		}
-	}
+	color: #fff;
 }
 
 h4 {
+	padding: 0.3rem 0;
+	margin: 0 !important;
 	font-size: 15px;
 	color: #fff;
 }
 
+.vote i {
+	width: 10px;
+	display: inline list-item;
+	color: yellow;
+}
+
 .image img {
 	width: 50%;
+	padding: 0.3rem 0;
 }
 </style>
