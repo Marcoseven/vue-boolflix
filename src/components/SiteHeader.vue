229130f7c122a86a77eac9954b7cace264b45820
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-24 16:50:01 +0100

    Aggiunte modifiche file vue (SiteHeader)
    Modificata una classe e cancellato un importazione.

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="row m-3">
+	<div class="SiteHeader m-3">
 		<h1 class="text-center">BoolFlix</h1>
 		<h2 class="text-center">Film, serie TV e tanto altro. Senza limiti.</h2>
 	</div>
@@ -10,7 +10,6 @@ export default {};
 </script>
 
 <style lang="scss">
-@import "../assets/scss/variables.scss";
 @import "../assets/scss/common.scss";
 h1 {
 	color: red;
